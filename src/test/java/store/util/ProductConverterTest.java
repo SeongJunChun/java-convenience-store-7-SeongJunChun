6f471d60ba5366129b116697b05ede7f10e5e4da
SeongJunChun <csj991210@naver.com>
2024-11-07 01:54:06 +0900

    feat: 객체, 문자열 변환기능 테스트

---
+++ src/test/java/store/util/ProductConverterTest.java
@@ -0,0 +1,28 @@
+package store.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.domain.Product;
+
+class ProductConverterTest {
+
+    @DisplayName("문자열을 Product 객체로 변환한다")
+    @Test
+    void convertToProduct() {
+        ProductConverter productConverter = new ProductConverter();
+        Product product = productConverter.convertToObject("콜라,1000,10,탄산2+1");
+        assertThat(product.getName()).isEqualTo("콜라");
+    }
+
+    @DisplayName("Product 객체를 문자열로 변환한다")
+    @Test
+    void convertToString() {
+        ProductConverter productConverter = new ProductConverter();
+        Product product = new Product("오렌지 에이드",1500,9,null);
+        String line = productConverter.convertToString(product);
+        assertThat(line).isEqualTo("오렌지 에이드,1500,9,null");
+    }
+
+}
