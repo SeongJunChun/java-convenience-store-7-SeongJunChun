ac3ccc80b134c1385244a7b8f7eb5e512c71047d
SeongJunChun <csj991210@naver.com>
2024-11-08 23:57:44 +0900

    refactor: 상품 클래스 빌더 패턴으로 변경

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,18 +1,14 @@
 package store.domain;
 
+import java.time.LocalDate;
+import store.domain.Promotion.Builder;
+
 public class Product {
 
     private final String name;
     private final int price;
     private final int quantity;
     private final String promotion;
-
-    public Product(String name, int price, int quantity, String promotion) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.promotion = promotion;
-    }
 
     public String getName() {
         return name;
@@ -28,6 +24,44 @@ public class Product {
 
     public String getPromotion() {
         return promotion;
+    }
+
+    public static class Builder {
+        private String name;
+        private int price;
+        private int quantity;
+        private String promotion;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder price(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder quantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public Builder promotion(String promotion) {
+            this.promotion = promotion;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+    }
+
+    private Product(Builder builder) {
+        this.name = builder.name;
+        this.price = builder.price;
+        this.quantity = builder.quantity;
+        this.promotion = builder.promotion;
     }
 
 }
