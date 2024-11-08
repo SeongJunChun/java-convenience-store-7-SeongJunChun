e76c261623ad8df3b34e85a69a39f17d63ffb7e2
SeongJunChun <csj991210@naver.com>
2024-11-09 01:18:40 +0900

    test: 프로모션 변환기능 테스트

---
+++ src/test/java/store/util/PromotionConverterTest.java
@@ -0,0 +1,36 @@
+package store.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.domain.Product;
+import store.domain.Promotion;
+
+class PromotionConverterTest {
+    @DisplayName("문자열을 Promotion 객체로 변환한다")
+    @Test
+    void convertToPromotion() {
+        PromotionConverter promotionConverter = new PromotionConverter();
+        Promotion promotion =promotionConverter.convertToObject("MD추천상품,1,1,2024-01-01,2024-12-31");
+        assertThat(promotion.getName()).isEqualTo("MD추천상품");
+    }
+
+    @DisplayName("Promotion 객체를 문자열로 변환한다")
+    @Test
+    void convertToString() {
+        PromotionConverter promotionConverter = new PromotionConverter();
+        Promotion promotion = new Promotion.Builder()
+                .name("MD추천상품")
+                .purchaseCount(1)
+                .bonusItemCount(1)
+                .startDay(LocalDate.of(2024,1,1))
+                .endDay(LocalDate.of(2024,12,31))
+                .build();
+        String line = promotionConverter.convertToString(promotion);
+        assertThat(line).isEqualTo("MD추천상품,1,1,2024-01-01,2024-12-31");
+    }
+
+}
