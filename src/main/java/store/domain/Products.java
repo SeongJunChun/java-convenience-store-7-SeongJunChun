6858354ae4cec19051d278b322d86e9a09757f50
SeongJunChun <csj991210@naver.com>
2024-11-09 19:13:22 +0900

    feat: 모든 상품을 관리하는 기능 구현

---
+++ src/main/java/store/domain/Products.java
@@ -0,0 +1,65 @@
+package store.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Products {
+
+    private final List<Product> products;
+    private static Products instance;
+
+    private Products(List<Product> products) {
+        this.products = List.copyOf(getAllProducts(products));
+    }
+
+    public static Products getInstance(List<Product> products) {
+        if (instance == null) {
+            instance = new Products(products);
+        }
+        return instance;
+    }
+
+    public static Products getInstance() {
+        if (instance == null) {
+            instance = new Products(List.of());
+        }
+        return instance;
+    }
+
+    public List<Product> findAllProductsByPromotion(Set<Promotion> promotions) {
+        return products.stream()
+                .filter(product -> promotions.contains(product.getPromotion()) || product.getPromotion() == null)
+                .toList();
+    }
+
+    private List<Product> getAllProducts(List<Product> products) {
+        Set<String> nonPromotionProducts = getNonPromotionProductNames(products);
+        List<Product> newProducts = new ArrayList<>();
+        products.forEach(product -> {
+            newProducts.add(product);
+            if (product.getPromotion() != null && !nonPromotionProducts.contains(product.getName())) {
+                newProducts.add(createNonPromotionProducts(product));
+            }
+        });
+        return newProducts;
+    }
+
+    private Product createNonPromotionProducts(Product product) {
+        return new Product.Builder()
+                .name(product.getName())
+                .price(product.getPrice())
+                .quantity(0)
+                .promotion(null)
+                .build();
+    }
+
+    private Set<String> getNonPromotionProductNames(List<Product> products) {
+        return products.stream()
+                .filter(product -> product.getPromotion() == null)
+                .map(Product::getName)
+                .collect(Collectors.toSet());
+    }
+
+}
