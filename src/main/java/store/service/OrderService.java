9b3a6c0bc29a5434f6812d65edc55aebcba92cf8
SeongJunChun <csj991210@naver.com>
2024-11-10 15:08:55 +0900

    feat: 상품의 주문을 담당하는 서비스 구현

---
+++ src/main/java/store/service/OrderService.java
@@ -0,0 +1,45 @@
+package store.service;
+
+import store.domain.OrderResult;
+import store.domain.product.AvailableProducts;
+import store.domain.product.Product;
+import store.domain.product.Products;
+
+public class OrderService {
+    private final Products products;
+    private final OrderResult orderResult;
+    private final AvailableProducts availableProducts;
+
+    public OrderService(Products products, OrderResult orderResult, AvailableProducts availableProducts) {
+        this.availableProducts = availableProducts;
+        this.products = Products.getInstance();
+        this.orderResult = orderResult;
+    }
+
+    public void buyNonPromotionProduct(String name, int amount) {
+        Product nonPromotionProduct = availableProducts.findNonPromotionProduct(name);
+        Product product = new Product.Builder()
+                .name(nonPromotionProduct.getName())
+                .price(nonPromotionProduct.getPrice())
+                .quantity(amount)
+                .promotion(null)
+                .build();
+
+        orderResult.addNonPromotionalProduct(product);
+        products.updateProduct(product);
+    }
+
+    public void buyPromotionProduct(String name, int amount) {
+        Product promotionProduct = availableProducts.findPromotionProduct(name);
+        Product product = new Product.Builder()
+                .name(promotionProduct.getName())
+                .price(promotionProduct.getPrice())
+                .quantity(amount)
+                .promotion(promotionProduct.getPromotion())
+                .build();
+
+        orderResult.addPromotionalProduct(product);
+        products.updateProduct(product);
+    }
+
+}
