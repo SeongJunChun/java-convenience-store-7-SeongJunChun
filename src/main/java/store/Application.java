3e6455befb0467cd0dd1504dd4d315b6288eca6d
SeongJunChun <csj991210@naver.com>
2024-11-11 03:38:01 +0900

    feat: 편의점 프로그램 실행 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,10 @@
 package store;
 
+import store.application.StoreApplication;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StoreApplication storeApplication = new StoreApplication();
+        storeApplication.run();
     }
 }
---
+++ src/main/java/store/application/StoreApplication.java
@@ -0,0 +1,17 @@
+package store.application;
+
+import store.controller.InputController;
+import store.controller.StoreController;
+import store.view.InputView;
+import store.view.OutputView;
+
+public class StoreApplication {
+    public void run(){
+        InputView inputView = new InputView();
+        InputController inputController = new InputController(inputView);
+        OutputView outputView = new OutputView();
+        StoreController storeController = new StoreController(inputController, outputView);
+
+        storeController.run();
+    }
+}
