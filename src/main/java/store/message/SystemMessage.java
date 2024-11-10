19b0b08f417717b621291d23e965865a9fa40f0c
SeongJunChun <csj991210@naver.com>
2024-11-11 03:34:09 +0900

    feat: 상품 출력 형식 변경

--- src/main/java/store/message/SystemMessage.java
+++ src/main/java/store/message/SystemMessage.java
@@ -1,9 +1,14 @@
 package store.message;
 
+import store.domain.product.Product;
+import store.domain.promotion.Promotion;
+
 public enum SystemMessage {
 
     WELCOME("안녕하세요. W편의점입니다."),
     CURRENT_PRODUCTS("현재 보유하고 있는 상품입니다."),
+    PRODUCT_FORMAT("- %s %s원 %s %s"),
+    HAS_NO_PRODUCTS("재고 없음"),
     ENTER_PRODUCT_AND_QUANTITY("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
     NON_PROMOTION_NOTICE("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
     FREE_ITEM_PROMPT("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
@@ -22,5 +27,24 @@ public enum SystemMessage {
 
     public String format(Object... args) {
         return String.format(message, args);
+    }
+
+    public static String productFormat(Product product) {
+        return PRODUCT_FORMAT.format(product.getName(), Receipt.formatPrice(product.getPrice()),
+                quantityFormat(product.getQuantity()), promotionFormat(product.getPromotion()));
+    }
+
+    private static String promotionFormat(Promotion promotion) {
+        if (promotion == null) {
+            return "";
+        }
+        return promotion.getName();
+    }
+
+    private static String quantityFormat(int quantity) {
+        if (quantity == 0) {
+            return HAS_NO_PRODUCTS.getMessage();
+        }
+        return quantity + "개";
     }
 }
