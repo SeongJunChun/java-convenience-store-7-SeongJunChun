b41496a3529f823ad593d9444b00b93b41af1e2b
SeongJunChun <csj991210@naver.com>
2024-11-10 21:30:47 +0900

    feat: 영수증 출력정보를 담은 enum 생성

---
+++ src/main/java/store/message/Receipt.java
@@ -0,0 +1,58 @@
+package store.message;
+
+import java.util.stream.Collectors;
+import store.dto.ReceiptDto;
+
+public enum Receipt {
+
+    HEADER("==============W 편의점================\n"),
+    ITEM_HEADER("상품명                수량      금액\n"),
+    ITEM("%-15s %8d %12s\n"), // 상품명 15칸 왼쪽 정렬, 수량 8칸 오른쪽 정렬, 금액 12칸 오른쪽 정렬
+    PROMOTION_HEADER("=============증    정===============\n"),
+    PROMOTION_ITEM("%-15s %8d\n"), // 상품명 15칸 왼쪽 정렬, 수량 8칸 오른쪽 정렬
+    FOOTER("====================================\n"),
+    TOTAL("총구매액         %8d %10s\n"), // 수량 8칸, 금액 12칸 오른쪽 정렬
+    EVENT_DISCOUNT("행사할인                 %11s\n"), // 할인 금액 12칸 오른쪽 정렬
+    MEMBERSHIP_DISCOUNT("멤버십할인              %12s\n"), // 할인 금액 12칸 오른쪽 정렬
+    FINAL_AMOUNT("내실돈                 %12s\n");
+    private final String format;
+
+    Receipt(String format) {
+        this.format = format;
+    }
+
+    public String format(Object... args) {
+        return String.format(format, args);
+    }
+
+    public static String formatReceipt(ReceiptDto receiptDto) {
+        return formatHeader(receiptDto) +
+                formatPromotionProducts(receiptDto) +
+                formatPrices(receiptDto);
+    }
+
+    private static String formatHeader(ReceiptDto receiptDto) {
+        return HEADER.format() +
+                ITEM_HEADER.format() +
+                receiptDto.getProducts().stream()
+                        .map(product -> ITEM.format(product.getName(), product.getQuantity(), product.getTotalPrice()))
+                        .collect(Collectors.joining());
+    }
+
+    private static String formatPromotionProducts(ReceiptDto receiptDto) {
+        return PROMOTION_HEADER.format() +
+                receiptDto.getPromotionProducts().entrySet().stream()
+                        .map(entry -> PROMOTION_ITEM.format(entry.getKey(), entry.getValue()))
+                        .collect(Collectors.joining());
+    }
+
+    private static String formatPrices(ReceiptDto receiptDto) {
+        return FOOTER.format() +
+                TOTAL.format(receiptDto.getTotalPriceAndQuantity().getTotalQuantity(),
+                        receiptDto.getTotalPriceAndQuantity().getTotalPrice()) +
+                EVENT_DISCOUNT.format(-receiptDto.getPromotionDiscount()) +
+                MEMBERSHIP_DISCOUNT.format(-receiptDto.getMembershipDiscount()) +
+                FINAL_AMOUNT.format(receiptDto.getTotalPayment());
+    }
+
+}
