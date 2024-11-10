1d56326ee64936d93c6ae2139a78c10818475119
SeongJunChun <csj991210@naver.com>
2024-11-11 03:36:11 +0900

    feat: oupput view 변경

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -7,6 +7,7 @@ import java.util.List;
 import store.domain.product.Product;
 import store.dto.ReceiptDto;
 import store.message.Receipt;
+import store.message.SystemMessage;
 
 public class OutputView {
 
@@ -14,8 +15,14 @@ public class OutputView {
         System.out.println(WELCOME.getMessage());
         System.out.println(CURRENT_PRODUCTS.getMessage());
         System.out.print(System.lineSeparator());
-        products.forEach(System.out::println);
+        printProduct(products);
         System.out.print(System.lineSeparator());
+    }
+
+    private void printProduct(List<Product> products) {
+        for(Product product : products) {
+            System.out.println(SystemMessage.productFormat(product));
+        }
     }
 
     public void printReceipt(ReceiptDto receipt) {
