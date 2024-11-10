1cf4d8631cc7b705787bd887dda4e9e4f090f20c
SeongJunChun <csj991210@naver.com>
2024-11-10 15:18:26 +0900

    feat: 할인 적용 및 금액 계산 기능 구현

--- src/main/java/store/domain/OrderResult.java
+++ src/main/java/store/domain/OrderResult.java
@@ -1,7 +1,11 @@
 package store.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import store.domain.product.Product;
 
 public class OrderResult {
@@ -12,6 +16,58 @@ public class OrderResult {
     public OrderResult() {
         nonPromotionalProducts = new ArrayList<Product>();
         promotionalProducts = new ArrayList<Product>();
+    }
+
+    public int calculateTotalPrice() {
+        return Stream.concat(nonPromotionalProducts.stream(), promotionalProducts.stream())
+                .mapToInt(p -> p.getPrice() * p.getQuantity())
+                .sum();
+    }
+
+    public int calculatePayment(Membership membership) {
+        return calculateTotalPrice() - calculatePromotionDiscount() - calculateMembershipDiscount(membership);
+    }
+
+    public Map<String, Integer> getProductSummary() {
+        Map<String, Integer> productSummary = new HashMap<>();
+
+        Stream.concat(nonPromotionalProducts.stream(), promotionalProducts.stream())
+                .forEach(product ->
+                        productSummary.merge(product.getName(), product.getQuantity(), Integer::sum)
+                );
+        return productSummary;
+    }
+
+    public Map<String, Integer> getPromotionSummary() {
+        return promotionalProducts.stream()
+                .filter(product -> product.getQuantity() >= product.getPromotion().getPromotionThreshold())
+                .collect(Collectors.toMap(
+                        Product::getName,
+                        product -> product.getQuantity() / product.getPromotion().getPromotionThreshold()
+                ));
+    }
+
+    public int calculatePromotionDiscount() {
+        return promotionalProducts.stream()
+                .filter(product -> product.getQuantity() >= product.getPromotion().getPromotionThreshold())
+                .mapToInt(product -> (product.getQuantity() / product.getPromotion().getPromotionThreshold())
+                        * product.getPrice())
+                .sum();
+    }
+
+    public int calculateMembershipDiscount(Membership membership) {
+        int nonPromotionPrice = calculateTotalPrice() - calculateTotalPromotionPrice();
+        return membership.calculateDiscount(nonPromotionPrice);
+    }
+
+    public int calculateTotalPromotionPrice() {
+        return promotionalProducts.stream()
+                .filter(product -> product.getQuantity() >= product.getPromotion().getPromotionThreshold())
+                .mapToInt(product -> {
+                    int nonPromotionQuantity = product.getQuantity() % product.getPromotion().getPromotionThreshold();
+                    return product.getPrice() * (product.getQuantity() - nonPromotionQuantity);
+                })
+                .sum();
     }
 
     public void addNonPromotionalProduct(Product product) {
