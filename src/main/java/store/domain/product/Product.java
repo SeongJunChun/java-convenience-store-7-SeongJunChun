4af4d1b05732a9296dd3c8e2e09580cf23fdb7e8
SeongJunChun <csj991210@naver.com>
2024-11-10 15:06:07 +0900

    feat: 상품 수량 감소 기능 구현

---
+++ src/main/java/store/domain/product/Product.java
@@ -0,0 +1,111 @@
+package store.domain.product;
+
+import store.domain.promotion.Promotion;
+
+public class Product {
+
+    private final String name;
+    private final int price;
+    private int quantity;
+    private final Promotion promotion;
+
+    private Product(Builder builder) {
+        this.name = builder.name;
+        this.price = builder.price;
+        this.quantity = builder.quantity;
+        this.promotion = builder.promotion;
+    }
+
+    public void reduceQuantity(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    @Override
+    public String toString() {
+        return String.join(" ",
+                name,
+                String.valueOf(price),
+                quantityToString(),
+                promotionToString()
+        );
+    }
+
+    private String promotionToString() {
+        if(promotion != null) {
+            return promotion.getName();
+        }
+        return "";
+    }
+
+    private String quantityToString() {
+        if(quantity == 0) {
+            return "제고 없음";
+        }
+        return String.valueOf(quantity);
+    }
+
+
+    public static class Builder {
+        private String name;
+        private int price;
+        private int quantity;
+        private Promotion promotion;
+
+        public Builder name(String name) {
+            validateBlank(name);
+            this.name = name;
+            return this;
+        }
+
+        public Builder price(int price) {
+            validatePositiveNumber(price);
+            this.price = price;
+            return this;
+        }
+
+        public Builder quantity(int quantity) {
+            validatePositiveNumber(quantity);
+            this.quantity = quantity;
+            return this;
+        }
+
+        public Builder promotion(Promotion promotion) {
+            this.promotion = promotion;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+
+        private void validatePositiveNumber(int number) {
+            if (number < 0) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        private void validateBlank(String name) {
+            if (name.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+    }
+
+}
